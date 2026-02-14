f4f8223f91afc0257e58341256a738eb0bd98de4
Dhayan2004 <dhay10azul@gmail.com>
2026-02-13 19:49:11 -0500

    limitedepost

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,26 @@ import entryRoutes from "./routes/routes";
 
 const app = express();
 
-const limiter = rateLimit({
+const postLimiter = rateLimit({
   windowMs: 2 * 60 * 1000, 
   max: 20, 
-  message: { error: "Has enviado demasiados registros. Intenta de nuevo en 5 minutos." },
-  standardHeaders: true,
-  legacyHeaders: false,
+  message: { error: "Has enviado demasiados registros. Intenta de nuevo en 2 minutos." },
+  standardHeaders: true, 
+  legacyHeaders: false, 
 });
 
 app.use(cors()); 
 app.use(express.json());
 
-app.use(limiter); 
-
-app.use("/entries", entryRoutes);
+app.use("/entries", (req, res, next) => {
+  if (req.method === "POST") {
+    return postLimiter(req, res, next);
+  }
+  next();
+}, entryRoutes);
 
 app.get("/", (req, res) => {
-  res.send("API de Dhayan funcionando y protegida contra spam");
+  res.send("API protegida");
 });
 
 const PORT = 3001; 
